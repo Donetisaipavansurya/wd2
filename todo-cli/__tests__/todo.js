39795b7563b626a5772333969f3f6cc411975a18
Nandini <donetisaipavansurya@gmail.com>
2023-12-20 00:17:08 +0530

    initial commit

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,32 +1,44 @@
 const todoList = require('../todo');
-const  {all, markAsComplete, add} = todoList();
-describe("Todolist Test Suite", () => {
-    beforeAll(() => {
-        add(
-            {
-                title: "Todo todo",
-                completed: false,
-                dueDate:  new Date().toLocaleDateString("en-CA")
-            }
-        );     
-    })
-    test("Should add new todo", () => {
-        const todoItemCount = all.length;
-        add(
-            {
-                title: "Todo todo",
-                completed: false,
-                dueDate:  new Date().toLocaleDateString("en-CA")
-            }
-        );
-        expect(all.length).toBe(todoItemCount +1);
-    });
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
-    test('Should mark a todo as complete', () => {
-        // Your setup code here
-        expect(all[0].completed).toBe(false);
-        markAsComplete('Todo todo'); // Use the correct title here
-        expect(all[0].completed).toBe(true);
-      });
-      
-})
+describe("Todolist Test Suite", () => {
+  beforeAll(() => {
+    // Add a todo for testing
+    add({
+      title: "Test Todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+
+  test("Should add new todo", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "New Todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
+
+  test('Should mark a todo as complete', () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete('Test Todo');
+    expect(all[0].completed).toBe(true);
+  });
+
+  test('Should retrieve overdue items', () => {
+    const overdueItems = overdue();
+    expect(overdueItems.length).toBeGreaterThanOrEqual(0);
+  });
+
+  test('Should retrieve due today items', () => {
+    const dueTodayItems = dueToday();
+    expect(dueTodayItems.length).toBeGreaterThanOrEqual(0);
+  });
+
+  test('Should retrieve due later items', () => {
+    const dueLaterItems = dueLater();
+    expect(dueLaterItems.length).toBeGreaterThanOrEqual(0);
+  });
+});
